31dc2ad0f2ef53c16b09c89b026b456b9016a4dd
Stacey Teltser <stacey.teltser@gmail.com>
2020-02-25 11:54:16 -0600

    added content and js

---
+++ js/app.js
@@ -0,0 +1,54 @@
+'use strict';
+//Do first.
+console.log('This is working!');
+
+// var userName = prompt('What is your name?');
+// console.log(userName);
+
+// var lowerCaseName = userName.toLowerCase();
+
+// if(lowerCaseName === 'stacey'){
+//     alert('Welcome back ' + userName + '!');
+// } else {
+//     alert('Welcome ' + userName + '!');
+// }
+
+// var answerOne = confirm('Do you want to begin your coding journey?');
+
+// if (answerOne === true || answerOne !== false ){
+//     alert('Let\'s go on a journey through time and space.');
+//     console.log('space time is going on');
+// } else {
+//     console.log('ok stay grounded')
+// }
+
+// var myBooleanValueTrue = false;
+// var anotherBooleanValueFalse = false;
+// var thirdBooleanValueTrue = true;
+
+// if(myBooleanValueTrue && thirdBooleanValueTrue){
+//     alert('the && will run if both conditions are true.');
+// } else if(myBooleanValueTrue || anotherBooleanValueFalse){
+//     alert('The || will  run if one or both of the values return true.');
+//     console.log('it is truthy enough...');
+// }
+
+alert('Welcome to space ship repair 101');
+var spaceHandle = prompt('what is your space handle?');
+var spaceShipPart = prompt('what spaceship part do you need?');
+
+//write the switch logic
+
+switch (spaceShipPart){
+    case '1':
+        console.log('This is space ship part one.');
+    break;
+    case '2': 
+        console.log('This is space ship part one.');
+    break;
+    case '3':
+        console.log('This is space ship part one.');
+    break;
+    default:
+        alert('I dont think we have your parts.');
+    }
